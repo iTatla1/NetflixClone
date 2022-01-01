4be82d294430b1fb1a1258747833a0676f39ca2e
Muhammad Usman Tatla <usm.gh36@gmail.com>
2022-01-01 09:15:40 +0500

    TabPicker

--- NetflixClone/MovieDetails/CustomTabSwitcher.swift
+++ NetflixClone/MovieDetails/CustomTabSwitcher.swift
@@ -8,36 +8,24 @@
 import SwiftUI
 
 struct CustomTabSwitcher: View {
-    var tabs: [CustomTab]
+    @State private var currentTab: CustomTab = .episodes
     
-    func widthForTab(_ tab: CustomTab) -> CGFloat {
-        return tab.rawValue.widthForString(usingFont: .systemFont(ofSize: 16, weight: .bold))
-    }
+    var tabs: [CustomTab]
     
     var body: some View {
         VStack(alignment: .leading) {
             // Scrollable Custom Tab Picker
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(tabs, id: \.self) { tab in
-                        VStack {
-                            Rectangle()
-                                .frame(width: widthForTab(tab), height: 6)
-                            Button {
-                                //
-                            } label: {
-                                Text(tab.rawValue)
-                                    .font(.system(size: 16, weight: .bold))
-
-                            }
-                            .buttonStyle(PlainButtonStyle())
-                        }
-                    }
-                }
-            }
+            TabPicker(tabs: tabs, currentTab: $currentTab)
             
             // Selected View
-            Text("Selected View")
+            switch currentTab {
+            case .episodes:
+                Text("Episodes")
+            case .trailers:
+                Text("Trailers")
+            case .more:
+                Text("More")
+            }
         }
         .foregroundColor(.white)
     }
@@ -57,5 +45,43 @@ struct CustomTabSwitcher_Previews: PreviewProvider {
                 .edgesIgnoringSafeArea(.all)
             CustomTabSwitcher(tabs: [.episodes, .trailers, .more])
         }
+    }
+}
+
+struct TabPicker: View {
+    let tabs: [CustomTab]
+    @Binding var currentTab: CustomTab
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            ScrollViewReader { value in
+                HStack(spacing: 20) {
+                    ForEach(tabs, id: \.self) { tab in
+                        VStack {
+                            Rectangle()
+                                .frame(width: widthForTab(tab), height: 6)
+                                .foregroundColor(tab == currentTab ? .red: .clear)
+                            Button {
+                                currentTab = tab
+                                value.scrollTo(tab, anchor: .trailing)
+                            } label: {
+                                Text(tab.rawValue)
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(tab == currentTab ? .white: .gray)
+                                
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                            .frame(height: 30)
+                        }
+                        .id(tab)
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func widthForTab(_ tab: CustomTab) -> CGFloat {
+        return tab.rawValue.widthForString(usingFont: .systemFont(ofSize: 16, weight: .bold))
     }
 }
